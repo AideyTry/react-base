0ffdd13e254f9385da3e88d8ebbc308c9826407b
jason19901028 <jason_dlb@sina.cn>
2019-03-27 08:54:03 +0800

    使用this.setState

--- src/components/BindEvent.jsx
+++ src/components/BindEvent.jsx
@@ -3,19 +3,25 @@ import React from 'react'
 export default class BindEvent extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      msg: '嘿嘿'
+    }
   }
   render () {
     return <div>
       <p>BindEvent组件</p>
       <hr />
       {/* 注意：在React中，有一套自己的事件绑定机制；事件名是驼峰命名；onClick只接受function作为函数处理。箭头函数本身就是匿名的function函数。 */}
-      <button onClick={() => { this.myclickHandler() }}>点击事件</button>
+      <button onClick={() => this.myclickHandler('猪头','美女')}>点击事件</button>
+      <h2>{this.state.msg}</h2>
     </div>
   }
 
   // 这是一个实例方法
-  myclickHandler () {
-    console.log('点击事件')
+  myclickHandler (arg1, arg2) {
+    // 在React中，推荐使用this.setState({})修改状态值
+    this.setState({
+      msg: arg1 + arg2
+    })
   }
 }
